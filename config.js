aa08094de4cbd222d3d4f7e4a45ac96438ab7173
coldrainN <75039007+coldrainN@users.noreply.github.com>
2026-02-14 15:19:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Катруся",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Будешь ли ты моей валентинкой? 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
